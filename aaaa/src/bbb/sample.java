504357747d7b286edfb2f71e1eb432895937fe84
venkataswami <51942690+vvswami@users.noreply.github.com>
2020-11-23 16:38:37 +0530

    fourth added

--- aaaa/src/bbb/sample.java
+++ aaaa/src/bbb/sample.java
@@ -6,10 +6,13 @@ public class sample {
 		int a = 10;
 		int b = 20;
 		int c;
+		int d;
 
 		c = a + b;
+		d=a+b+c;
 		System.out.println(c);
 		System.out.println("print successfully");
+		
 			
 	}
 
