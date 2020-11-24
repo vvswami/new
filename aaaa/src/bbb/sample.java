3bb2634e0eb2f4cbf91196003d323e22dc72c3da
venkataswami <51942690+vvswami@users.noreply.github.com>
2020-11-24 10:53:25 +0530

    c remove

--- aaaa/src/bbb/sample.java
+++ aaaa/src/bbb/sample.java
@@ -5,16 +5,9 @@ public class sample {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
-		int c;
-		int d;
-
-		c = a + b;
-		d=a+b+c;
+	
 		System.out.println(c);
-		System.out.println("print successfully");
 		
-
-		System.out.println("Today i have lean create branch and pull and push in github" );
 		
 		
 		
